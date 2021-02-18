46941eee0713e13d03f4c05ee3069408c1a909d7
nom0relife <vaitsis_vagias@hotmail.com>
2021-02-19 01:16:20 +0200

    faq

---
+++ src/components/FaqSection.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+const FaqSection = () => {
+  return (
+    <div className="faq">
+      <h2>
+        Any questions <span>FAQ</span>
+        <div className="question">
+          <h4>How to start</h4>
+          <div className="answer">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            magnam quo cupiditate expedita commodi repellat. Alias mollitia,
+            quam quos, sit aliquid quis illum commodi atque aliquam unde minus
+            consequuntur deleniti? Quis voluptates officia placeat error eaque
+            velit enim, est et facere, a amet facilis eligendi doloribus porro
+            ex! Ex hic tempore tenetur cupiditate atque, recusandae dolor dicta
+            aliquam porro non?
+          </div>
+        </div>
+        <div className="question">
+          <h4>Daily</h4>
+          <div className="answer">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            magnam quo cupiditate expedita commodi repellat. Alias mollitia,
+            quam quos, sit aliquid quis illum commodi atque aliquam unde minus
+            consequuntur deleniti? Quis voluptates officia placeat error eaque
+            velit enim, est et facere, a amet facilis eligendi doloribus porro
+            ex! Ex hic tempore tenetur cupiditate atque, recusandae dolor dicta
+            aliquam porro non?
+          </div>
+        </div>
+        <div className="question">
+          <h4>Payment</h4>
+          <div className="answer">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            magnam quo cupiditate expedita commodi repellat. Alias mollitia,
+            quam quos, sit aliquid quis illum commodi atque aliquam unde minus
+            consequuntur deleniti? Quis voluptates officia placeat error eaque
+            velit enim, est et facere, a amet facilis eligendi doloribus porro
+            ex! Ex hic tempore tenetur cupiditate atque, recusandae dolor dicta
+            aliquam porro non?
+          </div>
+        </div>
+        <div className="question">
+          <h4>How am i</h4>
+          <div className="answer">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+            magnam quo cupiditate expedita commodi repellat. Alias mollitia,
+            quam quos, sit aliquid quis illum commodi atque aliquam unde minus
+            consequuntur deleniti? Quis voluptates officia placeat error eaque
+            velit enim, est et facere, a amet facilis eligendi doloribus porro
+            ex! Ex hic tempore tenetur cupiditate atque, recusandae dolor dicta
+            aliquam porro non?
+          </div>
+        </div>
+      </h2>
+    </div>
+  );
+};
+
+export default FaqSection;
