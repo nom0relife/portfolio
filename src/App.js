d753b97bfaa43d234858eb59b0c228d891f18d0e
nom0relife <vaitsis_vagias@hotmail.com>
2021-02-19 02:30:02 +0200

    styling styled components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 // import pages
 import AboutUs from "./pages/AboutUs";
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
