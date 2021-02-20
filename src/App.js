a98ff3af8b214e1c8319a555d7a10e3a12a3d12e
nom0relife <vaitsis_vagias@hotmail.com>
2021-02-20 03:39:51 +0200

    link,Route,Switch

--- src/App.js
+++ src/App.js
@@ -3,12 +3,28 @@ import React from "react";
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
 import Nav from "./components/Nav";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+//router
+
+import { Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-      <Nav />
       <GlobalStyle />
-      <AboutUs />
+      <Nav />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work" exact>
+          <OurWork />
+        </Route>
+        <Route path="/contact" exact>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
